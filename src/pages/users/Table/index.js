4b62fbf110bd0f277d44fca7bf9a839bf22dea51
andyanhkali174 <andyanhkali174@kali-andy.andyanh>
2024-03-17 19:17:01 +0700

    add router homePages

--- src/pages/users/Table/index.js
+++ src/pages/users/Table/index.js
@@ -5,7 +5,12 @@ import "./style.scss";
 const Table = () => {
     return (
     <>
-    <iframe className="pdf-custom" src="https://drive.google.com/file/d/17-qL8PzIYFs6o6D0HR7WZ54tcnpc82Vp/preview" allow="autoplay"></iframe>
+    <div className="Table">
+        <div className="Table-custom">
+            
+        </div>
+
+    </div>
     </>
     
     );
--- src/pages/users/homePage/index.js
+++ src/pages/users/homePage/index.js
@@ -1,74 +1,76 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import BacHo from "assets/users/images/categories/BacHo.jpg" ;
-import PhamMinhChinh from "assets/users/images/categories/PhamMinhChinh.jpg" ;
-import VoNguyenGiap from "assets/users/images/categories/VoNguyenGiap.jpg" ;
-import NguyenPhuTrong from "assets/users/images/categories/NguyenPhuTrong.jpg" ;
+import BacHo from "assets/users/images/categories/BacHo.jpg";
+import PhamMinhChinh from "assets/users/images/categories/PhamMinhChinh.jpg";
+import VoNguyenGiap from "assets/users/images/categories/VoNguyenGiap.jpg";
+import NguyenPhuTrong from "assets/users/images/categories/NguyenPhuTrong.jpg";
 
 import "./style.scss";
 
 const HomePage = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 4
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
-      const sliderItems = [
-        {
-            bgimgL: BacHo,
-            name: "Bác Hồ"
-        },
-        {
-            bgimgL: VoNguyenGiap,
-            name: "Võ Nguyên Giáp"
-        },
-        {
-            bgimgL: PhamMinhChinh,
-            name: "Phạm Minh Chính"
-        },
-        {
-            bgimgL: NguyenPhuTrong,
-            name: "Nguyễn Phú Trọng"
-        },
-      ]
+  const sliderItems = [
+    {
+      bgimgL: BacHo,
+      name: "Bác Hồ",
+      iframeSrc: "https://tulieuvankien.dangcongsan.vn/c-mac-angghen-lenin-ho-chi-minh/ho-chi-minh/tieu-su-cuoc-doi-va-su-nghiep/tieu-su-chu-tich-ho-chi-minh-52",
+    },
+    {
+      bgimgL: VoNguyenGiap,
+      name: "Võ Nguyên Giáp",
+      iframeSrc: "https://tulieuvankien.dangcongsan.vn/c-mac-angghen-lenin-ho-chi-minh/ho-chi-minh/tieu-su-cuoc-doi-va-su-nghiep/tieu-su-chu-tich-ho-chi-minh-52",
+    },
+    {
+      bgimgL: PhamMinhChinh,
+      name: "Phạm Minh Chính",
+      iframeSrc: "https://www.qdnd.vn/chinh-tri/tin-tuc/tom-tat-tieu-su-thu-tuong-chinh-phu-pham-minh-chinh-666555",
+    },
+    {
+      bgimgL: NguyenPhuTrong,
+      name: "Nguyễn Phú Trọng",
+      iframeSrc: 'https://tulieuvankien.dangcongsan.vn/c-mac-angghen-lenin-ho-chi-minh/ho-chi-minh/tieu-su-cuoc-doi-va-su-nghiep/tieu-su-chu-tich-ho-chi-minh-52',
+    },
+  ];
 
-    return (
+  const handleClick = (iframeSrc) => {
+    window.location.href = iframeSrc;
+  };
+
+  return (
     <>
-    {/* Categories Begin */}
-    <div className="container container-categories-slider">
-
+      <div className="container container-categories-slider">
         <Carousel responsive={responsive} className="categories-slider">
-            {
-                sliderItems.map((item, key) => (
-            <div className="categories-slider-item" 
-            style={{ backgroundImage: `url(${item.bgimgL})`}}>
-               </div>
-
-                ))
-            }
+          {sliderItems.map((item, key) => (
+            <div
+              key={key}
+              className="categories-slider-item"
+              style={{ backgroundImage: `url(${item.bgimgL})` }}
+              onClick={() => handleClick(item.iframeSrc)}
+            ></div>
+          ))}
         </Carousel>
-    </div>
-    {/* Categories Begin */}
-    
+      </div>
     </>
-    
-    );
+  );
 };
 
 export default memo(HomePage);
